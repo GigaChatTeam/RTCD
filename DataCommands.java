f61c604e6c14200b30300c2cbb41166f33d7f775
mister-proger <84221561+mister-proger@users.noreply.github.com>
2023-09-13 13:45:01 +0300

    Redistributed simplified command classes

--- DataCommands.java
+++ DataCommands.java
@@ -3,10 +3,10 @@ public class DataCommands {
         public String type;
         public int id = 0;
         public int channel;
-        public int author;
     }
     public static class MessagePost extends Message {
         public String type = "MESSAGE-POST";
+        public int author;
         public String text;
     }
     public static class MessageDelete extends Message {
@@ -14,22 +14,18 @@ public class DataCommands {
     }
 
     private abstract static class ChannelControl {
-        public int id = 0;
-
+        public int id;
+        public int author;
     }
     public static class ChannelCreate extends ChannelControl {
         public String type = "CHANNEL-CONTROL-CREATE";
         public String title;
-
-        public ChannelCreate (int id) {
-            this.id = id;
-        }
     }
     private abstract static class ChannelUserControl extends ChannelControl {
         public int user;
     }
     public static class ChannelUserControlAccess extends ChannelUserControl {
-        public String type = "CHANNEL-USERCONTROL-ADD";
+        public String type = "CHANNEL-USERCONTROL-PRESENCE";
         public Boolean access;
     }
     public static class ChannelUserPermissions extends ChannelUserControl {
