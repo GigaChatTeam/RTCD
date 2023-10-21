db5972cd4f484bed65ed28e72ce34c093e4bec61
mister-proger <i@savnil.ru>
2023-10-21 23:52:12 +0300

    Deleted test code

--- Starter.java
+++ Starter.java
@@ -4,12 +4,8 @@ public class Starter {
     static byte DEBUG = 2;
 
     public static void main (String[] args) {
-//        int port = 8080;
-//        WSCore server = new WSCore(port);
-//        server.start();
-
-        ResponsesPatterns.Channels.Messages.Post.New test = new ResponsesPatterns.Channels.Messages.Post.New(1, 1, "test", "test", new Timestamp(System.currentTimeMillis()));
-
-        System.out.println(test.serialize(Helper.SHA512("123")));
+        int port = 8080;
+        WSCore server = new WSCore(port);
+        server.start();
     }
 }
