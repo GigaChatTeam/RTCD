02c3f2acbc1cc9944f4368fc3ebddc6d91bf89de
mister-proger <i@savnil.ru>
2023-11-20 17:24:16 +0300

    Edit connection port

--- Starter.java
+++ Starter.java
@@ -2,7 +2,7 @@ public class Starter {
     static byte DEBUG = 5;
 
     public static void main (String[] args) {
-        int port = 8000;
+        int port = 8080;
         WSCore server = new WSCore(port);
         server.start();
     }
