a864134a9e4bdf981e84488886bff728cf5669da
mister-proger <i@savnil.ru>
2023-11-20 17:17:08 +0300

    Added some administrative rights in channels

--- Commands.java
+++ Commands.java
@@ -1,42 +1,37 @@
 import java.util.Arrays;
+import java.util.Objects;
+
 
 public enum Commands {
-    ADMIN_CHANNELS_CREATE(new String[]{"ADMIN", "CHANNELS", "CREATE"}, CommandsPatterns.Channels.Create.class),
-    ADMIN_CHANNELS_DELETE(new String[]{"ADMIN", "CHANNELS", "DELETE"}, CommandsPatterns.Channels.Delete.class),
-    ADMIN_CHANNELS_USERS_ADD(new String[]{"ADMIN", "CHANNELS", "USERS", "ADD"}, CommandsPatterns.Channels.Users.Add.class),
-    ADMIN_CHANNELS_USERS_REMOVE(new String[]{"ADMIN", "CHANNELS", "USERS", "REMOVE"}, CommandsPatterns.Channels.Users.Remove.class),
+    // System channels management
+    CHANNELS_SYSTEM_CREATE("B3", null),
+    CHANNELS_SYSTEM_DELETE("B2", null),
+    // Group rights management
+    CHANNELS_ADMIN_PERMISSIONS_GROUPS_DELETE("5A4", null),
+    CHANNELS_ADMIN_PERMISSIONS_GROUPS_EDIT("5A5", null),
+    CHANNELS_ADMIN_PERMISSIONS_GROUPS_CREATE("5A6", null),
+    // User rights management
+    CHANNELS_ADMIN_PERMISSIONS_USERS_EDIT("2D0", null),
+    CHANNELS_ADMIN_PERMISSIONS_USERS_GROUPS("2D1", null),
+    // Administration of users
+    CHANNELS_ADMIN_USERS_ADD("2DC", null),
+    CHANNELS_ADMIN_USERS_MUTE("2DD", null),
+    CHANNELS_ADMIN_USERS_BAN("2DE", null),
+    CHANNELS_ADMIN_USERS_KICK("2DF", null),
+    // Changing external settings
+    CHANNELS_ADMIN_SETTINGS("B5", null);
 
-    ADMIN_CHANNELS_SETTINGS_EXTERNAL_CHANGE_TITLE(new String[]{"ADMIN", "CHANNELS", "SETTINGS", "EXTERNAL", "CHANGE", "TITLE"}, CommandsPatterns.Channels.Settings.External.Change.Title.class),
-    ADMIN_CHANNELS_SETTINGS_EXTERNAL_CHANGE_DESCRIPTION(new String[]{"ADMIN", "CHANNELS", "SETTINGS", "EXTERNAL", "CHANGE", "DESCRIPTION"}, CommandsPatterns.Channels.Settings.External.Change.Description.class),
-
-    USER_CHANNELS_JOIN(new String[]{"USER", "CHANNELS", "JOIN"}, CommandsPatterns.Channels.Users.Join.class),
-    USER_CHANNELS_LEAVE(new String[]{"USER", "CHANNELS", "LEAVE"}, CommandsPatterns.Channels.Users.Leave.class),
-
-    USER_CHANNELS_MESSAGES_POST_NEW(new String[]{"USER", "CHANNELS", "MESSAGES", "POST", "NEW"}, CommandsPatterns.Channels.Messages.Post.New.class),
-    USER_CHANNELS_MESSAGES_POST_FORWARD_MESSAGE(new String[]{"USER", "CHANNELS", "MESSAGES", "POST", "FORWARD", "MESSAGE"}, CommandsPatterns.Channels.Messages.Post.ForwardMessage.class),
-    USER_CHANNELS_MESSAGES_POST_FORWARD_POST(new String[]{"USER", "CHANNELS", "MESSAGES", "POST", "FORWARD", "POST"}, CommandsPatterns.Channels.Messages.Post.ForwardPost.class),
-    USER_CHANNELS_MESSAGES_EDIT(new String[]{"USER", "CHANNELS", "MESSAGES", "EDIT"}, CommandsPatterns.Channels.Messages.Edit.class),
-    USER_CHANNELS_MESSAGES_DELETE(new String[]{"USER", "CHANNELS", "MESSAGES", "DELETE"}, CommandsPatterns.Channels.Messages.Delete.class),
-
-    USER_CHANNELS_MESSAGES_REACTIONS_ADD(new String[]{"USER", "CHANNELS", "MESSAGES", "REACTIONS", "ADD"}, CommandsPatterns.Channels.Reactions.Add.class),
-    USER_CHANNELS_MESSAGES_REACTIONS_REMOVE(new String[]{"USER", "CHANNELS", "MESSAGES", "REACTIONS", "REMOVE"}, CommandsPatterns.Channels.Reactions.Remove.class),
-
-    SYSTEM_CHANNELS_LISTEN_ADD(new String[]{"SYSTEM", "CHANNELS", "LISTEN", "ADD"}, CommandsPatterns.Systems.Listen.Channel.Add.class),
-    SYSTEM_CHANNELS_LISTEN_REMOVE(new String[]{"SYSTEM", "CHANNELS", "LISTEN", "REMOVE"}, CommandsPatterns.Systems.Listen.Channel.Remove.class),
-
-    SYSTEM_TTOKENS_GENERATE(new String[]{"SYSTEM", "TTOKENS", "GENERATE"}, Helper.TTokenQueryWrapper.class);
-
-    final String[] intents;
+    final String intents;
     final Class<?> pattern;
 
-    Commands (String[] intents, Class<?> pattern) {
+    Commands (String intents, Class<?> pattern) {
         this.intents = intents;
         this.pattern = pattern;
     }
 
-    public static Commands byIntents (String[] intents) {
+    public static Commands byIntents (String intents) {
         return Arrays.stream(Commands.values())
-            .filter(v -> Arrays.equals(v.intents, intents))
+            .filter(v -> Objects.equals(v.intents, intents))
             .findFirst()
             .orElse(null);
     }
