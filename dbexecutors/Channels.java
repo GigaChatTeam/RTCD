72b29c371f336eb7c366373bcdbd681afd1a4aec
mister-proger <84221561+mister-proger@users.noreply.github.com>
2024-01-11 16:11:39 +0300

    Prepare commit

--- dbexecutors/Channels.java
+++ dbexecutors/Channels.java
@@ -32,6 +32,10 @@ public class Channels {
         }
     }
 
+    public static class Administration {
+
+    }
+
     public static class Users {
         public static class Presence {
             public static boolean isClientOnChannel (long client, long channel) throws SQLException {
